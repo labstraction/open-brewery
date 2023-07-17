5b17e8b6b1e8b49147ef96cd295fe2ed0f88e9dd
labstraction <45163263+labstraction@users.noreply.github.com>
2023-07-17 09:35:05 +0200

    div render function

--- main.js
+++ main.js
@@ -5,10 +5,32 @@ DBService.getBreweries(pageNumber)
 .then(breweries => render(breweries))
 
 function render(breweries){
+    const mainContainer = document.getElementById('main-container');
+    mainContainer.innerHTML = '';
+
 
     for (brewery of breweries) {
-        console.log(brewery.name)
-        
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('brewery-card');
+
+        const nameStrong = document.createElement('strong');
+        const nameNode = document.createTextNode(brewery.name);
+        nameStrong.appendChild(nameNode);
+        cardDiv.appendChild(nameStrong);
+
+        const addressDiv = document.createElement('div');
+
+        const citySpan = document.createElement('span');
+        const cityNode = document.createTextNode(brewery.city);
+        citySpan.appendChild(cityNode);
+        addressDiv.appendChild(citySpan);
+
+        const stateSpan = document.createElement('span');
+        const stateNode = document.createTextNode(', ' + brewery.state);
+        stateSpan.appendChild(stateNode);
+        addressDiv.appendChild(stateSpan);
+        cardDiv.appendChild(addressDiv);
+        mainContainer.appendChild(cardDiv);
     }
 
 }
